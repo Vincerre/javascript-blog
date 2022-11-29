36cc125baef47bc19256400b97e7a1426a05d050
Patryk Bazior <bazior.patryk@gmail.com>
2022-11-29 16:34:03 +0100

    task 6.4

--- js/script.js
+++ js/script.js
@@ -37,8 +37,48 @@ const titleClickHandler = function(event){
 
 }
 
-const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  function clearMessages(){
+    titleList.innerHTML = '';
+  }
+  clearMessages();
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+    for(let article of articles){
+      
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+
+    /* find the title element */
+    
+
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+    /* insert link into titleList */
+    html = html + linkHTML;
+    
+  }
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
+
+  for(let link of links){
   link.addEventListener('click', titleClickHandler);
+  }
 }
+
+generateTitleLinks();
