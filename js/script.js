d8a1e3dad6a2e7306e512604a9419127ddfd95bc
Patryk Bazior <bazior.patryk@gmail.com>
2022-11-29 02:55:36 +0100

    article show on click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
@@ -22,11 +23,18 @@ const titleClickHandler = function(event){
    for(let activeArticle of activeArticles){
      activeArticle.classList.remove('active');
    }
-  /* get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('link clicked', articleSelector);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('clicked', targetArticle);
+
+  /* [DONE] add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
